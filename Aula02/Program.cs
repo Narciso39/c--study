e812fe4fcd10e368bc9a9b2b93d2b8406ad3d3a6
Luis Felipe <luis@witetec.com>
2026-06-22 10:59:14 -0300

    aula 02 -pt 2

---
+++ Aula02/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Aula02;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // comentarios de uma linha
+        /*
+         * comentarios de multiplas linhas
+            * mais de uma linha
+        */
+
+        string myName = "Luis Felipe";
+
+        int myAge = 23;
+
+        double myHeight = 1.79;
+
+        char firstCharacter = 'L';
+
+        bool isDeveloper = true;
+
+        long myPhoneNumber = 2199999999;
+
+        Console.WriteLine(myName);
+        Console.WriteLine(myAge);
+        Console.WriteLine(myHeight);
+        Console.WriteLine(firstCharacter);
+        Console.WriteLine(isDeveloper);
+        Console.WriteLine(myPhoneNumber);
+    }
+}
